285c148f87a1ed06629d89aa3027ea36056e5a11
Vishnu-1702 <vishnuvk1702@gmail.com>
2026-06-21 12:06:50 +0530

    update vite.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/my-portfolio/', // Make sure this line is exactly here with both slashes
   plugins: [
     react(),
     tailwindcss(),
